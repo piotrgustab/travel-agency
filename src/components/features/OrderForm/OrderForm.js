c517395848fab74cb2469604c1484eec6e7deba9
Piotr Gustab <pgustab@aol.com>
2021-08-19 22:00:18 -0400

    Show names

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Row, Col} from 'react-flexbox-grid';
 
+import pricing from '../../../data/pricing.json';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import OrderOption from '../OrderOption/OrderOption';
+
+import {Row, Col} from 'react-flexbox-grid';
 
 const OrderForm = ({tripCost, options}) => {
   return (
     <Row>
+      {pricing.map(option => (
+        <Col key={option.id} md={4}>
+          <OrderOption {...option}/>
+        </Col>
+      ))}
       <Col xs={12}>
         <OrderSummary cost={tripCost} options={options}/>
       </Col>
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './OrderOption.scss';
+
+const OrderOption = ({name}) => {
+  return (
+    <div className={styles.component}>
+      <h3 className={styles.title}>{name}</h3>
+    </div>
+  );
+};
+
+OrderOption.propTypes = {
+  name: PropTypes.string,
+};
+
+export default OrderOption;
